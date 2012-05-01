e1e7716a301377d9552675b9b617ac3592e0bba4
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/GoUptoTocPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GoUptoTocPlugin/build.pl
+++ lib/Foswiki/Plugins/GoUptoTocPlugin/build.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl -w
 
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 
-$build = new Foswiki::Contrib::Build( "GoUptoTocPlugin" );
-$build->build($build->{target});
+$build = new Foswiki::Contrib::Build("GoUptoTocPlugin");
+$build->build( $build->{target} );
 
