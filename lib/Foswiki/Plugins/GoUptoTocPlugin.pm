7015ecf598c999ed051cbf2c3fedc22655363ddd
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/GoUptoTocPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GoUptoTocPlugin.pm
+++ lib/Foswiki/Plugins/GoUptoTocPlugin.pm
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -wI.
 #
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # Based on parts of GenPDF, which has several sources and authors
@@ -14,9 +14,8 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
-
 
 # change the package name and $pluginName!!!
 package Foswiki::Plugins::GoUptoTocPlugin;
@@ -26,7 +25,8 @@ use strict;
 
 # $VERSION is referred to by Foswiki, and is the only global variable that
 # *must* exist in this package.
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use vars
+  qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
 
 # This should always be $Rev: 12445$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -40,25 +40,28 @@ $RELEASE = '0.2.2';
 
 # Short description of this plugin
 # One line description, is shown in the %FoswikiWEB%.TextFormattingRules topic:
-$SHORTDESCRIPTION = 'Adds a arrow to each header, which lets you jump up the TOC anchor';
+$SHORTDESCRIPTION =
+  'Adds a arrow to each header, which lets you jump up the TOC anchor';
 
 # Name of this Plugin, only used in this module
 $pluginName = 'GoUptoTocPlugin';
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # Plugin correctly initialized
     return 1;
 }
 
 sub preRenderingHandler {
+
     # do not uncomment, use $_[0], $_[1]... instead
     # if there is a TOC
-    
-    if( $_[0] =~ /.*a name="foswikiTOC".*/ ) {
-    	my $name = $Foswiki::cfg{Plugins}{GoUptoTocPlugin}{LinkName};    	
-	    $_[0] =~ s/(---[+]{1,4}\s+)(.*)/---$1$2 <a href="#foswikiTOC" class="GouptoTocLink" title="Click to scroll up to the table of contents" style="font-size:12px;text-decoration:none;color:blue">$name<\/a>/mgi;
+
+    if ( $_[0] =~ /.*a name="foswikiTOC".*/ ) {
+        my $name = $Foswiki::cfg{Plugins}{GoUptoTocPlugin}{LinkName};
+        $_[0] =~
+s/(---[+]{1,4}\s+)(.*)/---$1$2 <a href="#foswikiTOC" class="GouptoTocLink" title="Click to scroll up to the table of contents" style="font-size:12px;text-decoration:none;color:blue">$name<\/a>/mgi;
     }
 }
 1;
